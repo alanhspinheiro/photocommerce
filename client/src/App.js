00268fefe054615f8c014d852c2998bcc54e01fe
alanhspipnheiro <alanhspinheiro@gmail.com>
2024-01-09 19:21:22 -0300

    configurando strapi

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
+import React from "react";
 
-import {createBrowserRouter, RouterProvider, Outlet} from 'react-router-dom';
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
 //pages
-import Home from './pages/Home';
-import Products from './pages/Products';
-import ProdunctDetails from './pages/ProductDetails';
-import Search from './pages/Search';
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import ProdunctDetails from "./pages/ProductDetails";
+import Search from "./pages/Search";
 
 //components
 
-import Header from './components/Header';
-import Footer from './components/Footer';
-
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 //layout
 
 const Layout = () => {
   return (
-    <div>]
+    <div>
+      
       <Header />
       <Outlet />
       <Footer />
@@ -30,18 +30,20 @@ const router = createBrowserRouter([
   {
     element: <Layout />,
     children: [
-      {path: '/', element: <Home />}, 
-      {path: '/products/:id', element: <Products /> }, 
-      {path: '/product/:id', element: <ProductDetails /> },
-      {path: '/search', element: <Search /> },
+      { path: "/", element: <Home /> },
+      { path: "/products/:id", element: <Products /> },
+      { path: "/product/:id", element: <ProductDetails /> },
+      { path: "/search", element: <Search /> },
     ],
   },
 ]);
 
 const App = () => {
-  return <div>
-    <RouterProvider router={router} />
-  </div>;
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
 };
 
 export default App;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,11 @@
 import React from 'react';
+// useFetch hook
+
+import useFetch from '../hooks/useFetch.js';
 
 const Home = () => {
+  const {data} = useFetch('/products?populate=*');
+  console.log(data);
   return <div>Home</div>;
 };
 
